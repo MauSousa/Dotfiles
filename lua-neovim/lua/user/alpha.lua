93871328a8a98afa41c515dc6c9d0bc3a6c564be
Augusto Sousa <lagrange.tesla@gmail.com>
2022-07-24 20:31:21 -0500

    Add new neovim config

--- lua-neovim/lua/user/alpha.lua
+++ lua-neovim/lua/user/alpha.lua
@@ -3,7 +3,7 @@ if not status_ok then
   return
 end
 
-local dashboard = require("alpha.themes.dashboard")
+local dashboard = require "alpha.themes.dashboard"
 dashboard.section.header.val = {
     "                                                     ",
     "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
@@ -15,32 +15,24 @@ dashboard.section.header.val = {
     "                                                     ",
 }
 dashboard.section.buttons.val = {
-  dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-  dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button("p", "  Update plugins", ":e ~/.config/nvim/lua/user/plugins.lua <CR> :w <CR>"),
-  dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-  dashboard.button("b", "  Bspwm config", ":e ~/.config/bspwm/bspwmrc <CR>"),
-  dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-  dashboard.button("df", "  Dotfiles", ":e ~/Documents/dotfiles/ <CR>"),
-  dashboard.button("du", "  St", ":e ~/.config/suckless/st-0.8.5/config.h <CR>"),
-  dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+  dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+  dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
+  dashboard.button("P", " " .. " Update plugins", ":e ~/.config/nvim/lua/user/plugins.lua<CR> :w<CR>"),
+  dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+  dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
+  dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+  dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
+-- local function footer()
+--   return "chrisatmachine.com"
+-- end
 
-local function footer()
--- NOTE: requires the fortune-mod package to work
-  -- local handle = io.popen("fortune")
-  -- local fortune = handle:read("*a")
-  -- handle:close()
-  -- return fortune
-  return "Credits to: chrisatmachine github.com/LunarVim/Neovim-from-scratch"
-end
-
-dashboard.section.footer.val = footer()
+-- dashboard.section.footer.val = footer()
 
 dashboard.section.footer.opts.hl = "Type"
 dashboard.section.header.opts.hl = "Include"
 dashboard.section.buttons.opts.hl = "Keyword"
 
 dashboard.opts.opts.noautocmd = true
--- vim.cmd([[autocmd User AlphaReady echo 'ready']])
 alpha.setup(dashboard.opts)
