ca3fae8d00c6598de50f39b52bf6207d51e385a8
Augusto Sousa <lagrange.tesla@gmail.com>
2022-12-19 14:14:04 -0600

    Add bar for third monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -10,5 +10,6 @@ polybar-msg cmd quit
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar bar3 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..." 
