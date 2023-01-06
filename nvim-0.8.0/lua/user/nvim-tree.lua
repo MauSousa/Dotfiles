3afe362ff795f48353f0a9d0db53fc60941c2b98
Augusto Sousa <lagrange.tesla@gmail.com>
2023-01-06 00:10:02 -0600

    Add key to spli in tree view

--- nvim-0.8.0/lua/user/nvim-tree.lua
+++ nvim-0.8.0/lua/user/nvim-tree.lua
@@ -59,8 +59,9 @@ nvim_tree.setup({
 		mappings = {
 			list = {
 				{ key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-				{ key = "h", cb = tree_cb("close_node") },
+				-- { key = "h", cb = tree_cb("close_node") },
 				{ key = "v", cb = tree_cb("vsplit") },
+				{ key = "h", cb = tree_cb("split") },
 			},
 		},
 	},
