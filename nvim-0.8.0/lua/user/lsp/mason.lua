1da63cac3938774c975d0110fdc148b995f9dc59
Augusto Sousa <lagrange.tesla@gmail.com>
2023-01-19 22:33:33 -0600

    Change icons

--- nvim-0.8.0/lua/user/lsp/mason.lua
+++ nvim-0.8.0/lua/user/lsp/mason.lua
@@ -13,13 +13,13 @@ local settings = {
 	ui = {
 		border = "none",
 		icons = {
-			package_installed = "◍",
-			package_pending = "◍",
-			package_uninstalled = "◍",
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
 		},
 	},
 	log_level = vim.log.levels.INFO,
-	max_concurrent_installers = 4,
+	max_concurrent_installers = 5,
 }
 
 require("mason").setup(settings)
